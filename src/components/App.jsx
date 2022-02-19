ef8ec031a39e9272a4aec29477abd95dbb4931f5
Igor Napolov <isnapolov@gmail.com>
2022-02-19 13:03:19 +0200

    localStorage
    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,8 +50,20 @@ class App extends React.Component {
     this.setState({filter: event.currentTarget.value})
    };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
   
-  
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
 
